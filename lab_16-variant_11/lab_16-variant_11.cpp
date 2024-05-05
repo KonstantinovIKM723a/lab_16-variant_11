aa0d9124fc119a8524a992902885c19bbf716688
KonstantinovIKM723a <alex7wrt@gmail.com>
2024-05-05 15:59:20 +0300

    Update

--- lab_16-variant_11/lab_16-variant_11.cpp
+++ lab_16-variant_11/lab_16-variant_11.cpp
@@ -1,20 +1,67 @@
-// lab_16-variant_11.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include "windows.h"
+using namespace std;
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class File {
+    string name;
+    int size;
+    string creationDate;
+    int assignCount;
+    public: 
+        File(): name(""), size(0), creationDate(""), assignCount(0) {}
+        File(string n, int s, string c, int a): name(n), size(s), creationDate(c), assignCount(a) {}
+        string getName() const { return name;}
+        int getSize() const { return size; }
+        string getCreationDate() const { return creationDate; }
+        int getAssignCount() const { return assignCount; }
+        void showFileInfo();
+};
+
+void File::showFileInfo() {
+    cout << "Файл \"" << name << "\":" << endl;
+    cout << "Розмір: " << size << endl;
+    cout << "Дата створення: " << creationDate << endl;
+    cout << "Кількість звернень: " << assignCount << endl;
+    cout << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    const int n = 3;
+    File Files[n];
+    Files[0] = File("Red.jpg", 220045, "24-04-2024", 2);
+    Files[1] = File("Green.jpg", 190760, "30-04-2024", 3);
+    Files[2] = File("Blue.jpg", 165478, "04-05-2024", 4);
+
+    cout << "Список файлів: " << endl << endl;
+    for (int i = 0; i < n; i++) Files[i].showFileInfo();
+
+
+    cout << "Список файлів, упорядкований в алфавітному порядку: " << endl << endl;
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = i; j < n - 1; j++) {
+            if (Files[j].getName() > Files[j + 1].getName()) {
+                File tempFile = Files[j];
+                Files[j] = Files[j + 1];
+                Files[j + 1] = tempFile;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) Files[i].showFileInfo();
+
+
+    cout << "Список файлів, розмір яких перевищує заданий: " << endl;
+    cout << "Введіть розмір файла: ";
+    int fileSize;
+    cin >> fileSize;
+    for (int i = 0; i < n; i++) if (Files[i].getSize() > fileSize) Files[i].showFileInfo();
+
+    cout << "Список файлів, число звернень до яких перевищує ліміт: " << endl;
+    cout << "Введіть ліміт звернень: ";
+    int assignCount;
+    cin >> assignCount;
+    for (int i = 0; i < n; i++) if (Files[i].getAssignCount() > assignCount) Files[i].showFileInfo();
+}
